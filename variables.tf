7c44a1a9f28ebda6ac580bea28fc75971b4f4d1a
Mike Peachey <mike.peachey@bjss.com>
2016-08-12 21:21:37 +0100

    Peering PoC

--- variables.tf
+++ variables.tf
@@ -4,12 +4,13 @@ variable "environment" {
   default     = "testenv"
 }
 
-variable "peer_vpcs" {
-  type        = "map"
-  description = "Map of 'aws_account_id => vpc_id,cidr_block' with which to create peering connections and output a map of cidr_blocks to connection ids"
-  default     = {
-    "0123456789012" = "vpc-99999999999999999,172.16.0.0/16"
-  }
+variable "peer_matrix" {
+  type        = "list"
+  description = "List of peering partner tuples (['peer_aws_account_id,peer_vpc_id,peer_cidr_block']) with which to create vpc peering connections"
+  default     = [
+    "0123456789012,vpc-99999999999999999,172.16.0.0/16",
+    "0123456789011,vpc-99999999999999998,172.32.0.0/16"
+  ]
 }
 
 variable "project" {
--- vpc_peering_connections.tf
+++ vpc_peering_connections.tf
@@ -1,12 +1,12 @@
 resource "aws_vpc_peering_connection" "vpc_peering_connection" {
-  count         = "${length(keys(var.peer_vpcs))}"
+  count         = "${length(var.peer_matrix)}"
   auto_accept   = true
-  peer_owner_id = "${element(keys(var.peer_vpcs), count.index)}"
-  peer_vpc_id   = "${element(split(",", lookup(var.peer_vpcs, element(keys(var.peer_vpcs), count.index))), 0)}"
+  peer_owner_id = "${element(split(",", element(var.peer_matrix, count.index)), 0)}"
+  peer_vpc_id   = "${element(split(",", element(var.peer_matrix, count.index)), 1)}"
   vpc_id        = "${var.source_vpc_id}"
 
   tags {
-    Name        = "${var.environment}-${var.project}-${element(split(",", lookup(var.peer_vpcs, element(keys(var.peer_vpcs),count.index))), 0)}-PEER"
+    Name        = "${var.environment}-${var.project}-${element(split(",", element(var.peer_matrix, count.index)), 1)}-PEER"
     Environment = "${var.environment}"
     Project     = "${var.project}"
     Tier        = "PRI"
