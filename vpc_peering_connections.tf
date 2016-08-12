aba879eddd8f08a7e8dcbe3acb541ef7b9cb5723
Mike Peachey <mike.peachey@bjss.com>
2016-08-12 18:12:40 +0100

    auto_accept

--- vpc_peering_connections.tf
+++ vpc_peering_connections.tf
@@ -1,5 +1,6 @@
 resource "aws_vpc_peering_connection" "vpc_peering_connection" {
   count         = "${length(keys(var.peer_vpcs))}"
+  auto_accept   = true
   peer_owner_id = "${var.source_aws_account_id}"
   peer_vpc_id   = "${lookup(var.peer_vpcs, element(keys(var.peer_vpcs), count.index))}"
   vpc_id        = "${var.source_vpc_id}"
