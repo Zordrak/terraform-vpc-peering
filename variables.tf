c994d101a529f471c28b17b2f136e82a5b4c9329
Mike Peachey <mike.peachey@bjss.com>
2016-08-12 19:13:45 +0100

    stash

--- variables.tf
+++ variables.tf
@@ -1,25 +1,25 @@
 variable "environment" {
   type        = "string"
   description = "Source VPC environment name"
+  default     = "testenv"
 }
 
 variable "peer_vpcs" {
   type        = "map"
-  description = "Map of 'aws_account_id=>vpc_ids' with which to create peering connections"
-  default     = {}
+  description = "Map of 'aws_account_id => vpc_id,cidr_block' with which to create peering connections and output a map of cidr_blocks to connection ids"
+  default     = {
+    "0123456789012" = "vpc-99999999999999999,172.16.0.0/16"
+  }
 }
 
 variable "project" {
   type        = "string"
   description = "Source VPC project name"
-}
-
-variable "source_aws_account_id" {
-  type        = "string"
-  description = "Source AWS AccountID"
+  default     = "testproj"
 }
 
 variable "source_vpc_id" {
   type        = "string"
   description = "Source/Owner VPC ID"
+  default     = "test_source_vpc_id_value"
 }
--- vpc_peering_connections.tf
+++ vpc_peering_connections.tf
@@ -1,12 +1,12 @@
 resource "aws_vpc_peering_connection" "vpc_peering_connection" {
   count         = "${length(keys(var.peer_vpcs))}"
   auto_accept   = true
-  peer_owner_id = "${var.source_aws_account_id}"
-  peer_vpc_id   = "${lookup(var.peer_vpcs, element(keys(var.peer_vpcs), count.index))}"
+  peer_owner_id = "${element(keys(var.peer_vpcs), count.index)}"
+  peer_vpc_id   = "${element(split(",", lookup(var.peer_vpcs, element(keys(var.peer_vpcs), count.index))), 0)}"
   vpc_id        = "${var.source_vpc_id}"
 
   tags {
-    Name        = "${var.environment}-${var.project}-${lookup(var.peer_vpcs, element(keys(var.peer_vpcs),count.index))}-PEER"
+    Name        = "${var.environment}-${var.project}-${element(split(",", lookup(var.peer_vpcs, element(keys(var.peer_vpcs),count.index))), 0)}-PEER"
     Environment = "${var.environment}"
     Project     = "${var.project}"
     Tier        = "PRI"
